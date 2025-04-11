8b4316eaf59fa9fc859e0c053e5e987616108552
Joachim Brand <joachim.brand@gmail.com>
2025-04-11 22:37:02 +1200

    Package version and module name

--- src/RimuRealTime.jl
+++ src/RimuRealTime.jl
@@ -1,5 +1,19 @@
 module RimuRealTime
 
+using Rimu
+import TOML
+
+const PACKAGE_NAME = "RimuRealTime"
+const PACKAGE_VERSION = VersionNumber(TOML.parsefile(pkgdir(RimuRealTime, "Project.toml"))["version"])
+
+@doc """
+    RimuRealTime
+`RimuRealTime` is a package for simulating many-body quantum systems in real time.
+
+Welcome to `RimuRealTime` version $PACKAGE_VERSION!
+"""
+RimuRealTime
+
 # Write your package code here.
 
 end
