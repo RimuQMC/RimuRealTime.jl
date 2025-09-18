2c5ae29afdb85eaf05d4d68f91d52f336669b43c
jamie-tay <jamiet96@gmail.com>
2025-09-18 18:07:00 +1200

    Fix fSim operator

--- src/fSim.jl
+++ src/fSim.jl
@@ -36,14 +36,14 @@ end
 
 Rimu.starting_address(c::fSimColumn) = c.address
 Rimu.parent_operator(c::fSimColumn) = c.op
-Rimu.num_offdiagonals(c::fSimColumn) = 2*num_occupied_modes(c.address)
+Rimu.num_offdiagonals(c::fSimColumn{M}) where {M} = 2^((M-c.even)÷2)-1
 
 function Rimu.diagonal_element(c::fSimColumn{M}) where {M}
-    result = 1
+    result = 1.0
     for i in 1+c.even:2:M-1
         if c.onr[i] == 1
             if c.onr[i+1] == 1
-                result *= exp(im*c.ϕ)
+                result *= exp(-im*c.ϕ)
             else
                 result *= cos(c.θ)
             end
@@ -57,66 +57,89 @@ function Rimu.diagonal_element(c::fSimColumn{M}) where {M}
 end
 
 function Rimu.random_offdiagonal(c::fSimColumn{M}) where {M}
-    pair = rand(1:(M-c.even)÷2)
-    site1 = 2*pair - 1 + c.even
-    site2 = 2*pair + c.even
-    if c.onr[site1] == 1 && c.onr[site2] == 0
-        #newadd, v = hopnextneighbour(c.address, 2*site1 - 1, :hard_wall)
-        newadd, v = excitation(c.address, (find_mode(c.address, site2),), (find_mode(c.address, site1),))
-        return newadd, 1/((M-c.even)÷2), im*sin(c.θ)
-    elseif c.onr[site2] == 1 && c.onr[site1] == 0
-        #newadd, v = hopnextneighbour(c.address, 2*site2, :hard_wall)
-        newadd, v = excitation(c.address, (find_mode(c.address, site1),), (find_mode(c.address, site2),))
-        return newadd, 1/((M-c.even)÷2), im*sin(c.θ)
+    newadd = starting_address(c)
+    val = 1.0+0.0im
+    diag = true
+    flips = rand(1:2^((M-c.even)÷2)-1)
+    for pair in 1:(M-c.even)÷2
+        site1 = 2*pair - 1 + c.even
+        site2 = 2*pair + c.even
+        if c.onr[site1] == 1 && c.onr[site2] == 1
+            val *= exp(-im*c.ϕ)
+        elseif flips & 2^(pair-1) == 2^(pair-1)
+            if c.onr[site1] == 1 && c.onr[site2] == 0
+                newadd, v = excitation(newadd, (find_mode(c.address, site2),), (find_mode(c.address, site1),))
+                val *= im*sin(c.θ)
+                diag = false
+            elseif c.onr[site2] == 1 && c.onr[site1] == 0
+                newadd, v = excitation(newadd, (find_mode(c.address, site1),), (find_mode(c.address, site2),))
+                val *= im*sin(c.θ)
+                diag = false
+            end
+        end
+    end
+    if diag
+        return newadd, 1/(2^((M-c.even)÷2)-1), 0.0im
     else
-        return c.address, 1/((M-c.even)÷2), 0.0
+        return newadd, 1/(2^((M-c.even)÷2)-1), val
     end
 end
 
 struct fSimOD{M,A<:FermiFS{<:Any,M},S}
     address::A
     θ::Float64
+    ϕ::Float64
     onr::S
     even::Bool
 end
-Rimu.offdiagonals(c::fSimColumn) = fSimOD(c.address, c.θ, c.onr, c.even)
+Rimu.offdiagonals(c::fSimColumn) = fSimOD(c.address, c.θ, c.ϕ, c.onr, c.even)
 
 Base.IteratorSize(::fSimOD{M}) where {M} = Base.HasLength()
 Base.eltype(::fSimOD{<:Any,A}) where {A} = Pair{A,ComplexF64}
-Base.size(o::fSimOD{M}) where {M} = ((M - o.even)÷2,)
+Base.size(::fSimOD{M}) where {M} = Base.SizeUnknown()
 
-function Base.iterate(o::fSimOD{M}) where {M}
-    pair = 1
-    if 2*pair > M - o.even
+function Base.iterate(o::fSimOD{M}, flips=0) where {M}
+    flips += 1
+    if flips >= 2^((M-o.even)÷2)
         return nothing
     end
-    site1 = 2*pair - 1 + o.even
-    site2 = 2*pair + o.even
-    if o.onr[site1] == 1 && o.onr[site2] == 0
-        newadd, v = excitation(o.address, (find_mode(o.address, site2),), (find_mode(o.address, site1),))
-        return newadd => im*sin(o.θ), pair
-    elseif o.onr[site2] == 1 && o.onr[site1] == 0
-        newadd, v = excitation(o.address, (find_mode(o.address, site1),), (find_mode(o.address, site2),))
-        return newadd => im*sin(o.θ), pair
-    else
-        return o.address => 0.0, pair
+    newadd = o.address
+    val = 1.0+0.0im
+    diag = true
+    repeat = false
+    for pair in 1:(M-o.even)÷2
+        site1 = 2*pair - 1 + o.even
+        site2 = 2*pair + o.even
+        if o.onr[site1] == 1 && o.onr[site2] == 1
+            val *= exp(-im*o.ϕ)
+            if flips & 2^(pair-1) == 2^(pair-1)
+                repeat = true
+                break
+            end
+        elseif o.onr[site1] == 0 && o.onr[site2] == 0 && flips & 2^(pair-1) == 2^(pair-1)
+            repeat = true
+            break
+        elseif o.onr[site1] == 1 && o.onr[site2] == 0
+            if flips & 2^(pair-1) == 2^(pair-1)
+                newadd, v = excitation(newadd, (find_mode(o.address, site2),), (find_mode(o.address, site1),))
+                val *= im*sin(o.θ)
+                diag = false
+            else
+                val *= cos(o.θ)
+            end
+        elseif o.onr[site2] == 1 && o.onr[site1] == 0
+            if flips & 2^(pair-1) == 2^(pair-1)
+                newadd, v = excitation(newadd, (find_mode(o.address, site1),), (find_mode(o.address, site2),))
+                val *= im*sin(o.θ)
+                diag = false
+            else
+                val *= cos(o.θ)
+            end
+        end
     end
-end
-
-function Base.iterate(o::fSimOD{M}, pair) where {M}
-    pair += 1
-    if 2*pair > M - o.even
-        return nothing
-    end
-    site1 = 2*pair - 1 + o.even
-    site2 = 2*pair + o.even
-    if o.onr[site1] == 1 && o.onr[site2] == 0
-        newadd, v = excitation(o.address, (find_mode(o.address, site2),), (find_mode(o.address, site1),))
-        return newadd => im*sin(o.θ), pair
-    elseif o.onr[site2] == 1 && o.onr[site1] == 0
-        newadd, v = excitation(o.address, (find_mode(o.address, site1),), (find_mode(o.address, site2),))
-        return newadd => im*sin(o.θ), pair
+    if diag || repeat
+        return newadd => 0.0im, flips
     else
-        return o.address => 0.0, pair
+        return newadd => val, flips
     end
 end
